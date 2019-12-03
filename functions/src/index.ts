272656cdbe5738cf03bc77d9d68e6976251c3943
CharlesYWL <xlyin@ucdavis.edu>
2019-12-03 04:21:03 -0800

    delete hello  function, deploy on firebase host

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,5 @@
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!\n\n");
-});
-
 export const receiveLog = functions.https.onRequest((request, response) => {
   var email = request.get("Email");
   console.log(email);
