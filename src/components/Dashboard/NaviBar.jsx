9397c433f3bbfe53ac3302e0ea16a2d5e96e8545
CharlesYWL <xlyin@ucdavis.edu>
2019-12-03 01:29:53 -0800

    figure data pass, need to do drawer

--- src/components/Dashboard/NaviBar.jsx
+++ src/components/Dashboard/NaviBar.jsx
@@ -3,27 +3,32 @@ import "./styles.css";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { IconButton, Button, Avatar } from "@material-ui/core";
+import { IconButton, Button, Avatar, Menu, MenuItem } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import fire from "../../config/Fire";
-import { string } from "prop-types";
 
 class NaviBar extends Component {
   constructor(props) {
     super(props);
-    //{this.props.containerRef}
     this.logout = this.logout.bind(this);
     this.state = {
+      status: "",
       name: ""
     };
+    console.log("we get " + this.props.statusRef + " from parent");
+    if (this.props.statusRef === "dashboard") {
+      this.state.status = "Dashboard";
+    } else if (this.props.statusRef === "blacklist") {
+      this.state.status = "BlackList";
+    } else if (this.props.statusRef === "whitelist") {
+      this.state.status = "WhiteList";
+    }
   }
-
   logout() {
+    console.log("Logouting");
     fire.auth().signOut();
   }
-
   render() {
-    console.log(this.props.containerRef);
     this.state.name = this.props.containerRef.substring(0, 2);
     return (
       <div className="root">
@@ -35,10 +40,10 @@ class NaviBar extends Component {
               color="inherit"
               aria-label="menu"
             >
-              <MenuIcon />
+              <MenuIcon onClick={this.props.triggerParentUpdate} />
             </IconButton>
             <Typography variant="h6" className="title">
-              My Dashboard
+              {this.state.status}
             </Typography>
             <Avatar>{this.state.name}</Avatar>
             <Button color="inherit" onClick={this.logout}>
--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-dom";
 import fire from "../../config/Fire";
-import "./styles.less";
+import "./styles.css";
 import NaviBar from "./NaviBar";
 import DataDisplay from "./dataDisplay";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { Button } from "@material-ui/core";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.addToDB = this.addToDB.bind(this);
+    this.toggleDrawer = this.toggleDrawer.bind(this);
+    this.clickOnMenu = this.clickOnMenu.bind(this);
     this.state = {
+      contantStatus: "dashboard",
+      DrawerVisible: false,
       user: {},
       uid: "",
       email: "",
@@ -21,7 +32,18 @@ class Dashboard extends Component {
       console.log(user);
     });
   }
-
+  //componentDidMount() {}
+  toggleDrawer() {
+    this.setState({
+      DrawerVisible: !this.state.DrawerVisible
+    });
+  }
+  // this function will be called by NaviBar
+  clickOnMenu() {
+    this.setState({ DrawerVisible: true });
+    console.log("called from children");
+  }
+  //Below is use case of db
   addToDB() {
     this.state.db
       .collection(this.state.uid)
@@ -39,9 +61,13 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <div>
-        <NaviBar containerRef={this.state.email} />
-        <DataDisplay />
+      <div className="root">
+        <CssBaseline />
+        <NaviBar
+          containerRef={this.state.email}
+          statusRef={this.state.contantStatus}
+          triggerParentUpdate={this.clickOnMenu}
+        />
       </div>
     );
   }
