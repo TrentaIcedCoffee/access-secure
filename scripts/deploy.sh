9fa06402134bc861a9260bd39012ec64d832ebb8
TrentaIcedCoffee <jin@ucdavis.edu>
2019-12-22 05:41:39 +0000

    None

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 FNAME=${PWD##*/}
 
-zip -r --exclude=*build/* build/function.zip .
+zip -r build/function.zip . -x build/\* -x debug.js -x package.json -x package-lock.json
 aws lambda update-function-code --function-name $FNAME --zip-file fileb://build/function.zip
