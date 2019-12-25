a3fe230dcdcc2c6d13ebf3c1338c4ac96ed90479
TrentaIcedCoffee <jin@ucdavis.edu>
2019-12-25 10:22:13 +0000

    None

--- lambda/app_logs_post/index.js
+++ lambda/app_logs_post/index.js
@@ -11,46 +11,81 @@ admin.initializeApp({
 
 var db = admin.firestore();
 
-var resOf = (statusCode, message) => {
-    return {
-        statusCode: statusCode,
+var okOf = (body={}) => {
+    var res = {
+        statusCode: 200,
         body: {
-            message: message
+            message: 'success',
+            isBlocked: false
         }
     };
+    for (const [key, value] of Object.entries(body)) {
+        res.body[key] = value;
+    }
+    return res;
 };
 
-exports.handler = async (event) => {
-    var appId = event.params.path.app_id;
-    let isValidToken = await db.doc(`apps/${appId}`).get()
-        .then(doc => {
-            return doc.exists && doc.data().token === event['body-json'].token;
-        }).catch(() => {
-            return false;
-        });
-    if (!isValidToken) {
-        return resOf(400, 'app validation failed');
+const checkSpamming = async (appId, ip, curTs) => {
+    const inWhitelist = await db.doc(`apps/${appId}/whitelist/${ip}`).get()
+        .then((doc) => {
+            return doc.exists;
+        }).catch((err) => console.log(err));
+    if (inWhitelist) {
+        return false;
     }
+    
+    const isSpamming = await db.collection(`apps/${appId}/logs`)
+        .orderBy('timestamp', 'desc').offset(3).limit(1).get().then(qs => {
+            const curTime = curTs.toMillis();
+            const prevTime = qs.docs.length === 1 ? qs.docs[0].data().timestamp.toMillis() : 0;
+            return curTime - prevTime <= 1000;
+        }).catch((err) => console.log(err));
+    return isSpamming;
+};
+
+const validateToken = async (appId, token) => {
+    return db.doc(`apps/${appId}`).get().then(doc => {
+        return (doc.exists && doc.data().token === token);
+    });
+};
+
+const parseInput = (event) => {
+    var appId = event.params.path.app_id;
+    var token = event['body-json'].token;
     
     var data = event['body-json'];
     delete data.token;
     data.timestamp = admin.firestore.Timestamp.now();
+    return {appId: appId, token: token, data: data};
+};
+
+exports.handler = async (event) => {
+    let {appId, token, data} = parseInput(event);
     
-    var shouldBlock = await db.collection(`apps/${appId}/logs`)
-        .orderBy('timestamp', 'desc').offset(10).limit(1).get().then(qs => {
-            return qs.docs.length === 1 &&
-                   qs.docs[0].data().timestamp.toMillis() >= data.timestamp.toMillis() - 1000;
-        });
-        
-    if (shouldBlock) {
-        // TODO
-    }
-        
-        
-    // return db.collection(`apps/${event.params.path.app_id}/logs`).add(data)
-    //     .then((res) => {
-    //         return resOf(200, 'success');
-    //     }).catch((err) => {
-    //         return resOf(500, 'failed');
-    //     });
+    return validateToken(appId, token).then((isValidToken) => {
+        if (!isValidToken) {
+            return {
+                statusCode: 400,
+                body: {
+                    message: 'app not exists or token not valid'
+                }
+            };
+        }
+        return checkSpamming(appId, data.ip, data.timestamp)
+            .then((isSpamming) => {
+                if (isSpamming) {
+                    return db.collection(`apps/${appId}/blacklist`).add({
+                        ip: data.ip
+                    }).then((res) => {
+                        return okOf({
+                            message: 'ip blocked for spamming',
+                            isBlocked: true
+                        });
+                    });
+                } else {
+                    return db.collection(`apps/${appId}/logs`).add(data)
+                        .then((res) => okOf());
+                }
+            });
+    });
 };
