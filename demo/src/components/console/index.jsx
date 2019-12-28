8e7c31ce8482dafbb1b1560ac3f3ad716d6cf921
TrentaIcedCoffee <jin@ucdavis.edu>
2019-12-28 06:47:46 +0000

    None

---
+++ demo/src/components/console/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Modal, Form, Alert } from 'react-bootstrap';
+
+import firebase from './firebase/';
+
+import './styles.sass';
+
+class Console extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+    this.firebase = firebase;
+  };
+  
+  return 
+
+  
+}
+
+export default Console;
