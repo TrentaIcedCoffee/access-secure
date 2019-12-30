89f3687e844ad561277c2f5b7df24cb11fdffb3b
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-12-30 02:47:24 -0800

    cannot figure out middle thing

--- demo/src/components/console/cards/index.jsx
+++ demo/src/components/console/cards/index.jsx
@@ -12,7 +12,7 @@ class Cards extends React.Component {
     user
     db
   */
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -24,36 +24,38 @@ class Cards extends React.Component {
     };
     this.refreshApps();
   }
-  
+
   refreshApps = () => {
     this.props.db.collection('apps')
       .where('uid', '==', this.props.user.uid)
       .get().then((docs) => {
         const apps = [];
         docs.forEach((doc) => {
-          const {name, description} = doc.data();
+          const { name, description } = doc.data();
           apps.push({
             id: doc.id,
             name: name,
             description: description || ''
           });
         });
-        this.setState({apps: apps});
+        this.setState({ apps: apps });
       }).catch((err) => console.log(new DebugError(err.message)));
   }
-  
+
   appendErrs = (err) => {
     this.setState({
       errs: this.state.errs.concat(err)
-    }, () => {setTimeout(() => {
-      this.setState({
-        errs: this.state.errs.splice(1, this.state.errs.length-1)
-      });
-    }, 1000)});
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          errs: this.state.errs.splice(1, this.state.errs.length - 1)
+        });
+      }, 1000)
+    });
   }
-  
+
   createProject = async () => {
-    const {name, description} = this.state;
+    const { name, description } = this.state;
     const nameSet = new Set(this.state.apps.map(app => app.name));
     if (name === '') {
       throw DebugError('project name is required');
@@ -67,34 +69,34 @@ class Cards extends React.Component {
       token: Math.random().toString(36).substring(8)
     });
   }
-  
+
   changeInput = (e) => {
-    this.setState({[e.target.id]: e.target.value});
+    this.setState({ [e.target.id]: e.target.value });
   }
-  
+
   submitNewProject = () => {
     this.createProject().then(() => {
       this.refreshApps();
-      this.setState({isAdding: false});
+      this.setState({ isAdding: false });
     }).catch((err) => {
       this.appendErrs(err.message);
     });
   }
-  
+
   toggleModal = () => {
-    this.setState({isAdding: !this.state.isAdding});
+    this.setState({ isAdding: !this.state.isAdding });
   }
-  
+
   render = () => {
-    const {apps, isAdding, name, description, errs} = this.state;
-    const {changeInput, toggleModal, submitNewProject} = this;
+    const { apps, isAdding, name, description, errs } = this.state;
+    const { changeInput, toggleModal, submitNewProject } = this;
     return (
       <div className="cards">
         <Card className="card" onClick={() => toggleModal()}>
           <Card.Body>
             <div className="icon-plus">&#43;</div>
             <div className="text-plus">New Project</div>
-         </Card.Body>
+          </Card.Body>
         </Card>
         {apps.map((app, idx) => (
           <Link className="anchor" key={idx} to={`/console/apps/${app.id}`}>
@@ -102,12 +104,12 @@ class Cards extends React.Component {
               <Card.Body>
                 <Card.Title>{app.name}</Card.Title>
                 <Card.Text>{app.description}</Card.Text>
-             </Card.Body>
+              </Card.Body>
             </Card>
           </Link>
         ))}
         <Modal
-          show={isAdding} 
+          show={isAdding}
           onHide={() => toggleModal()}
         >
           <Modal.Header closeButton>
@@ -136,7 +138,7 @@ class Cards extends React.Component {
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button 
+            <Button
               variant="secondary"
               onClick={() => toggleModal()}>
               Close
