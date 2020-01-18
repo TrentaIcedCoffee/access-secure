66ec1ae6a01cd457c40ca2a3d9d55663b6c3cb59
TrentaIcedCoffee <jin@ucdavis.edu>
2020-01-18 04:21:48 +0000

    words

--- frontend/src/pages/doc.jsx
+++ frontend/src/pages/doc.jsx
@@ -10,7 +10,7 @@ class Doc extends React.Component {
       <div className="up">
         <div className="mid">
           <h1>Access Secure</h1>
-          <p>Access middleware as third-party service</p>
+          <p>Middlewares as third-party services</p>
           <ul>
             <li>Black/White List</li>
             <li>Anti-Spamming</li>
@@ -28,30 +28,25 @@ class Doc extends React.Component {
       <div className="down container">
         <div className="row">
           <div className="col-md-4">
-            <h2>Access Middleware as third-party service</h2>
-            <p>React-Bootstrap replaces the Bootstrap JavaScript. 
-              Each component has been built from scratch as a true React 
-              component, without unneeded dependencies like jQuery.</p>
-            <p>As one of the oldest React libraries, React-Bootstrap has 
-              evolved and grown alongside React, making it an excellent 
-              choice as your UI foundation.</p>
+            <h2>Middlewares as third-party services</h2>
+            <p>Middlewares such as logger, authentication, anti-spamming and ip 
+              black/white list has nothing to do with your app logic, thus we
+              extract these parts so you can focus on the business logic.</p>
           </div>
           <div className="col-md-4">
-            <h2>Integrated with various frameworks</h2>
-            <p>Built with compatibility in mind, we embrace our bootstrap core 
-              and strive to be compatible with the world's largest 
-              UI ecosystem.</p>
-            <p>By relying entirely on the Bootstrap stylesheet, React- 
-              Bootstrap just works with the thousands of 
-              bootstrap themes you already love.</p>
+            <h2>No extra infrastructures</h2>
+            <p>Extra infrastructures &#8594; one bundle. Your app could be 
+            simple as one backend and one database. With a centralized 
+            management system, you have your app fully under control.</p>
           </div>
           <div className="col-md-4">
-            <h2>Features</h2>
-            <p>The React component model gives us more control over
-              form and function of each component.</p>
-            <p>Each component is implemented with accessibility in mind. 
-              The result is a set of accessible-by-default components, 
-              over what is possible from plain Bootstrap.</p>
+            <h2>Features and Supports</h2>
+            <ul>
+              <li>Log</li><li>ip black/white list</li><li>anti-spamming</li>
+            </ul>
+            <ul>
+              <li>Express</li><li>Koa</li><li>Flask</li><li>Django</li>
+            </ul>
           </div>
         </div>
       </div>
