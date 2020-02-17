ae26c371f55c6231274fa08b49764db5a9201a24
TrentaIcedCoffee <jin@ucdavis.edu>
2020-02-17 00:10:11 +0000

    deploy.sh

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-FNAME=$1
-
-if [ "$FNAME" == "" ]
-then
-    echo "Missing function name"
-    exit 0
-fi
-
-cd funcs/$FNAME/
-zip -r ../../build/$FNAME.zip .
-cd ../../
-aws lambda update-function-code --function-name $FNAME --zip-file fileb://build/$FNAME.zip
-rm -f build/$FNAME.zip # generate new zip everytime
+cd ./index
+zip -r index.zip .
+aws lambda update-function-code --function-name index --zip-file fileb://index.zip
+rm -f ./index.zip
